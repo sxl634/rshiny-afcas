6f51702b9ce08024efb0a991a73fff64995de965
sxl634 <slongman66@gmail.com>
2023-04-04 21:37:31 +0100

    add renv

--- R/time-series-graph.R
+++ R/time-series-graph.R
@@ -1,6 +1,6 @@
 time_series_graph <- function(data, response, title) {
   plot <- ggplot(data = data, mapping = aes(x = Year, y = est, group = Officer_or_Rank, col = Officer_or_Rank)) +
-    geom_line(size = 1) + 
+    geom_line(linewidth = 1) + 
     scale_y_continuous(breaks = seq(0,100,10), limits = c(0,100), expand = c(0,0)) +
     scale_x_discrete(name = NULL, expand = c(0,0)) +
     expand_limits(y = 0) +
